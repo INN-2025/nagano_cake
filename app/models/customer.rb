2ce44ae1e0a40f57122a39604abd205c4d1d0940
nakamuray41104-byte <235420323+nakamuray41104-byte@users.noreply.github.com>
2025-11-17 19:52:55 +0900

    Revert "顧客登録/ログイン機能"

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,7 +3,4 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  validates :last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, presence: true
-  has_many :cart_items, dependent: :destroy
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,3 @@
 class Product < ApplicationRecord
   belongs_to :genre
-  has_many :cart_items, dependent: :destroy
 end
