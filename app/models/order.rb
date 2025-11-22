656510eaa5fdc69f57f3555c6f522a7306ce7d16
shiori1218soba-boop <235053419+shiori1218soba-boop@users.noreply.github.com>
2025-11-22 07:13:57 +0000

    エラー修正

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,17 @@
 class Order < ApplicationRecord
 
-  enum status:  [ :before_payment, :confirmed_payment, :making, :ready_for_shipment, :shipped]
+  enum status: {
+  before_payment: 0,
+  confirmed_payment: 1,
+  making: 2,
+  ready_for_shipment: 3,
+  shipped: 4
+}
+
+  def status_i18n
+    I18n.t("enums.order.status.#{status}")
+  end
+
 
   belongs_to :customer
   has_many :order_details
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,4 +1,12 @@
 class OrderDetail < ApplicationRecord
   belongs_to :order
   belongs_to :product
+
+
+
+  def subtotal
+    price * amount
+  end
+
 end
+
