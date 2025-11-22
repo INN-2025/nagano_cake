c2f1c6c96dd31411fc55f88fe8ad2157fa9bdb1a
nakamuray41104-byte <235420323+nakamuray41104-byte@users.noreply.github.com>
2025-11-22 06:56:59 +0000

    管理者機能：注文詳細画面とOrderDetailsController実装

---
+++ app/controllers/admin/order_details_controller.rb
@@ -0,0 +1,20 @@
+class Admin::OrderDetailsController < ApplicationController
+  before_action :authenticate_admin!
+  
+  def update
+    @order = Order.find(params[:order_id])
+    @order_detail = @order.order_details.find(params[:id])
+    
+    if @order_detail.update(order_detail_params)
+      redirect_to admin_order_path(@order), notice: '製作ステータスを更新しました。'
+    else
+      redirect_to admin_order_path(@order), alert: '更新に失敗しました。'
+    end
+  end
+  
+  private
+  
+  def order_detail_params
+    params.require(:order_detail).permit(:making_status)
+  end
+end
