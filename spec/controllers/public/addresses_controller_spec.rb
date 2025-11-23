4e315bee928897c01a85d5694f4b0f33c5e94888
miwa-1017 <ubuntu@ip-172-31-28-251.ap-northeast-1.compute.internal>
2025-11-23 07:45:19 +0000

    Add controller test: public (index/show)

---
+++ spec/controllers/public/addresses_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe Public::AddressesController, type: :controller do
+  describe "GET #index" do
+    it "returns a success response" do
+      customer = Customer.create!(
+        last_name: "山田", first_name: "太郎",
+        last_name_kana: "ヤマダ", first_name_kana: "タロウ",
+        postal_code: "1234567", address: "大阪市",
+        telephone_number: "00000000000",
+        email: "test@example.com", password: "password"
+      )
+
+      sign_in customer
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
